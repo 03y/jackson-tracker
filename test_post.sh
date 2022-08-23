d895cedfe04d05c535030191eaae2a1d45a019a5
03y <98747393+03y@users.noreply.github.com>
2022-08-23 21:13:01 +0100

    deploy

--- test_post.sh
+++ test_post.sh
@@ -7,6 +7,6 @@ echo "Type: $type_value"
 echo "Author = $author_value"
 echo "Time = $time_value"
 
-curl -X POST localhost:8080 \
+curl -X POST 192.168.1.125:3000 \
    -H 'Content-Type: application/json' \
    -d '{"type":"'$type_value'","time":"'$time_value'","author":"'$author_value'"}'
